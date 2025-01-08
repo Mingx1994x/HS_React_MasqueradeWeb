488a505dbed7b1b0cc878bc9fac355202e94c7c3
Mingx1994x <mggxmingx@gmail.com>
2025-01-09 02:00:21 +0800

    feat: 新增環境變數與新增登入功能

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,47 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
-  const [isLogin, setLogin] = useState(false);
-  const [count, setCount] = useState(0);
-  const baseUrl = 'https://ec-course-api.hexschool.io';
-  const api = 'yameow2024';
+  const [isLogin, setIsLogin] = useState(false);
+  const [account, setAccount] = useState(
+    {
+      username: "",
+      password: ""
+    }
+  );
+  const { VITE_APP_BaseUrl, VITE_APP_API } = import.meta.env;
   const getData = async () => {
     try {
-      const res = await axios.get(`${baseUrl}/v2/api/${api}/products/all`);
+      const res = await axios.get(`${VITE_APP_BaseUrl}/v2/api/${VITE_APP_API}/products/all`);
       console.log(res);
     } catch (error) {
-      console.log(error);
+      alert(error.response.data.message)
     }
   }
 
-  getData();
+  const signin = async () => {
+
+    try {
+      const res = await axios.post(`${VITE_APP_BaseUrl}/v2/admin/signin`, account)
+      console.log(res);
+      setAccount({
+        username: '',
+        password: ''
+      })
+      setIsLogin(true);
+
+    } catch (error) {
+      alert(error.response.data.message)
+    }
+  }
+
+  const inputHandler = (name, value) => {
+    setAccount({
+      ...account,
+      [name]: value
+    })
+
+    // console.log(account)
+  }
 
   return (
     <div className='container'>
@@ -29,18 +56,29 @@ function App() {
           <div className="loginPhase">
             <div className="row">
               <div className="col-md-6 col-lg-8">
-                <form className='card'>
+                <form className='card' onSubmit={(e) => {
+                  e.preventDefault();
+                  signin()
+                }}>
                   <div className="card-body">
                     <div className="mb-3">
                       <div className="form-floating">
-                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                        <input type="email" name='username' className="form-control" id="floatingInput"
+                          value={account?.username}
+                          placeholder="name@example.com" onChange={(e) => {
+                            inputHandler(e.target.name, e.target.value)
+                          }} />
                         <label htmlFor="floatingInput">Email</label>
                       </div>
                     </div>
 
                     <div className="mb-3">
                       <div className="form-floating">
-                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                        <input type="password" name='password' className="form-control" id="floatingPassword"
+                          value={account?.password}
+                          placeholder="Password" onChange={(e) => {
+                            inputHandler(e.target.name, e.target.value)
+                          }} />
                         <label htmlFor="floatingPassword">Password</label>
                       </div>
                     </div>
