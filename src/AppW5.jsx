57eddb71bc5835da83220d6b223be549646c45d4
Mingx1994x <mggxmingx@gmail.com>
2025-02-04 23:55:17 +0800

    feat: 新增商品查看細節Modal彈出/關閉功能

--- src/AppW5.jsx
+++ src/AppW5.jsx
@@ -1,24 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { Modal } from "bootstrap";
 
 const { VITE_APP_BaseUrl, VITE_APP_API } = import.meta.env;
 
 function AppW5() {
 	const [products, setProducts] = useState([]);
-	const [productsCategory, setProductsCategory] = useState([]);
+	let defaultData = {
+		category: "",
+		content: "",
+		description: "",
+		id: "",
+		is_enabled: "",
+		origin_price: "",
+		price: "",
+		title: "",
+		unit: "",
+		num: "",
+		imageUrl: "",
+		imagesUrl: [],
+	};
+	const [tempProduct, setTempProduct] = useState(defaultData);
+	const [tempQty, setTempQty] = useState(1);
 	const [cartsData, setCartsData] = useState([]);
+	const [productsCategory, setProductsCategory] = useState([]);
 
 	const getProducts = async () => {
 		try {
 			const res = await axios.get(
-				`${VITE_APP_BaseUrl}/api/${VITE_APP_API}/products`
+				`${VITE_APP_BaseUrl}/api/${VITE_APP_API}/products?page=2`
 			);
 			console.log(res);
 			setProducts(res.data.products);
 			getCategory(products)
 			setProductsCategory(getCategory(products));
 		} catch (error) {
-			console.log(error);
+			// console.log(error);
 		}
 	};
 
@@ -29,12 +47,25 @@ function AppW5() {
 			const res = await axios.get(
 				`${VITE_APP_BaseUrl}/api/${VITE_APP_API}/cart`
 			);
-			console.log(res);
+			// console.log(res);
 			setCartsData(res.data.data);
 		} catch (error) {
-			console.log(error);
+			// console.log(error);
 		}
 	};
+
+	const productCardModal = useRef(null)
+	const productCardModalRef = useRef(null);
+	const openModal = (product) => {
+		setTempProduct(product);
+		productCardModal.current = new Modal(productCardModalRef.current);
+		productCardModal.current.show();
+	}
+
+	const closeModal = () => {
+		productCardModal.current.hide();
+		setTempQty(1);
+	}
 
 	useEffect(() => {
 		getProducts();
@@ -51,20 +82,20 @@ function AppW5() {
 							<h2 className="section-title text-center">商品列表</h2>
 							<div className="my-2 row">
 								<div className="col-md-3">
-									<select name="" className="form-select">
+									{/* <select name="" className="form-select">
 										<option value="全部" selected>全部</option>
 										{
 											productsCategory.map(category => (
 												<option value="床架">{category}</option>
 											))
 										}
-									</select>
+									</select> */}
 								</div>
 							</div>
 							<ul className="row row-cols-2 row-cols-lg-3 g-2 g-lg-3">
 								{
 									products.map(product => {
-										const { id, title, imageUrl, price, content, category } = product;
+										const { id, title, imageUrl, origin_price, description, price, category } = product;
 										return (
 											<li className="col productItem" key={id}>
 												<span className="productCategory text-bg-warning rounded-2">{category}</span>
@@ -79,11 +110,12 @@ function AppW5() {
 														alt={title} />
 													<div className="card-body">
 														<h3 className="card-title">{title}</h3>
-														<p className="card-text">{`價格：${price}`}</p>
-														<p className="card-text">{content}</p>
+														<p className="card-text"><del>{`價格：${origin_price}`}</del></p>
+														<p className="card-text text-danger">{`特價：${price}`}</p>
+														<p className="card-text text-truncate">{`商品描述：${description}`}</p>
 													</div>
 													<div className="card-footer d-flex justify-content-center">
-														<button type="button" className="btn btn-outline-primary rounded-end-0">查看細節</button>
+														<button type="button" className="btn btn-outline-primary rounded-end-0" onClick={() => openModal(product)}>查看細節</button>
 														<button type="button" className="btn btn-outline-danger rounded-start-0">加入購物車</button>
 													</div>
 												</div>
@@ -93,6 +125,47 @@ function AppW5() {
 								}
 							</ul>
 						</section>
+						<div className="modal" tabIndex="-1" ref={productCardModalRef}>
+							<div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+								<div className="modal-content">
+									<div className="modal-header">
+										<h3 className="modal-title">{tempProduct.title}</h3>
+										<button type="button" className="btn-close" onClick={closeModal}></button>
+									</div>
+									<div className="modal-body">
+										<img
+											src={tempProduct.imageUrl}
+											className="card-img-top img-fluid"
+											alt={tempProduct.title} />
+										<p className="mt-3">{`商品類別：${tempProduct.category}`}</p>
+										<p className="mt-3">{`商品${tempProduct.content}`}</p>
+										<p className="mt-3">{`商品描述：${tempProduct.description}`}</p>
+										<p className="mt-3">{`商品原價：${tempProduct.origin_price}`}</p>
+										<p className="mt-3 text-danger">{`搶購特價：${tempProduct.price}`}</p>
+										<div className="d-flex align-items-center mt-3">
+											購買數量：
+											<button type="button" className="btn btn-sm btn-outline-secondary ms-3" disabled={tempQty === 1 ? true : false} onClick={() => {
+												setTempQty(tempQty - 1)
+											}}>-</button>
+											<span className="mx-2">{tempQty}</span>
+											<button type="button" className="btn btn-sm btn-outline-warning" onClick={() => {
+												setTempQty(tempQty + 1)
+											}}>+</button>
+										</div>
+									</div>
+									<div className="modal-footer">
+										<button
+											type="button"
+											className="btn btn-secondary" onClick={closeModal}>取消
+										</button>
+										<button
+											type="button"
+											className="btn btn-danger">加入購物車
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
 						<section className="section shoppingCart">
 							<div className="row d-flex justify-content-center">
 								<div className="col-12">
