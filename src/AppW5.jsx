294d718c8a5cd6297065cdadf8c22e31b39e8915
Mingx1994x <mggxmingx@gmail.com>
2025-02-09 00:55:00 +0800

    refactor: 拆分商品Modal元件

--- src/AppW5.jsx
+++ src/AppW5.jsx
@@ -5,6 +5,7 @@ import { Modal } from "bootstrap";
 import { useForm } from "react-hook-form";
 import HashLoader from "react-spinners/HashLoader"
 import ProductCard from "./components/ProductCard";
+import ProductCardModal from "./components/ProductCardModal";
 
 const { VITE_APP_BaseUrl, VITE_APP_API } = import.meta.env;
 const customerUrl = `${VITE_APP_BaseUrl}/api/${VITE_APP_API}`;
@@ -26,7 +27,7 @@ function AppW5() {
 		imagesUrl: [],
 	};
 	const [tempProduct, setTempProduct] = useState(defaultData);
-	const [tempQty, setTempQty] = useState(1);
+	// const [tempQty, setTempQty] = useState(1);
 	const [cartsData, setCartsData] = useState({});
 	const [cartStatus, setCartStatus] = useState(false);
 	const [selectState, setSelectState] = useState('');
@@ -184,10 +185,10 @@ function AppW5() {
 		productCardModal.current.show();
 	}
 
-	const closeModal = () => {
-		productCardModal.current.hide();
-		setTempQty(1);
-	}
+	// const closeModal = () => {
+	// 	productCardModal.current.hide();
+	// 	setTempQty(1);
+	// }
 
 	useEffect(() => {
 		if (selectState === '') {
@@ -247,50 +248,12 @@ function AppW5() {
 								}
 							</ul>
 						</section>
-						<div className="modal" tabIndex="-1" ref={productCardModalRef}>
-							<div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-								<div className="modal-content">
-									<div className="modal-header">
-										<h3 className="modal-title">{tempProduct.title}</h3>
-										<button type="button" className="btn-close" onClick={closeModal}></button>
-									</div>
-									<div className="modal-body">
-										<img
-											src={tempProduct.imageUrl}
-											className="card-img-top img-fluid"
-											alt={tempProduct.title} />
-										<p className="mt-3">{`商品類別：${tempProduct.category}`}</p>
-										<p className="mt-3">{`商品${tempProduct.content}`}</p>
-										<p className="mt-3">{`商品描述：${tempProduct.description}`}</p>
-										<p className="mt-3">{`商品原價：${tempProduct.origin_price}`}</p>
-										<p className="mt-3 text-danger">{`搶購特價：${tempProduct.price}`}</p>
-										<div className="d-flex align-items-center mt-3">
-											購買數量：
-											<button type="button" className="btn btn-sm btn-outline-secondary ms-3" disabled={tempQty === 1 ? true : false} onClick={() => {
-												setTempQty(tempQty - 1)
-											}}><i className="bi bi-dash fs-5"></i></button>
-											<span className="mx-2">{tempQty}</span>
-											<button type="button" className="btn btn-sm btn-outline-warning" onClick={() => {
-												setTempQty(tempQty + 1)
-											}}><i className="bi bi-plus fs-5"></i></button>
-										</div>
-									</div>
-									<div className="modal-footer">
-										<button
-											type="button"
-											className="btn btn-secondary" onClick={closeModal}>取消
-										</button>
-										<button
-											type="button"
-											className="btn btn-danger" onClick={() => {
-												addCarts(tempProduct.id, tempQty);
-												closeModal()
-											}}>加入購物車
-										</button>
-									</div>
-								</div>
-							</div>
-						</div>
+						<ProductCardModal
+							tempProduct={tempProduct}
+							modalRef={productCardModalRef}
+							productCardModal={productCardModal}
+							addCarts={addCarts}
+						/>
 						{cartStatus ? (
 							<section className="section shoppingCart">
 								<div className="row d-flex justify-content-center">
